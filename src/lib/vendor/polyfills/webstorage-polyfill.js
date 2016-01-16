5279d7469e3268eda76a9c20d92b90fe41548fc1
Carlos Naranjo <canamo06@gmail.com>
2016-01-16 08:19:50 -0600

    #1 :: adding web storage wrapper to the repo, adding webstorage polyfill as fallback using cookies

---
+++ src/lib/vendor/polyfills/webstorage-polyfill.js
@@ -0,0 +1,144 @@
+/**                   _
+ *  _             _ _| |_
+ * | |           | |_   _|
+ * | |___  _   _ | | |_|
+ * | '_  \| | | || | | |
+ * | | | || |_| || | | |
+ * |_| |_|\___,_||_| |_|
+ *
+ * (c) Huli Inc
+ */
+
+
+/**
+ * @file Web storage polyfill
+ * @description Web storate polyfill, it uses cookies as a fallback
+ * @module ju-shared/lib/vendor/polyfills/webstorage-polyfill
+ * @author Unknown
+ */
+
+define([],
+        function() {
+
+    'use strict';
+
+    var webStoragePolyfill = {
+
+        /**
+         * Initialize the polyfill
+         */
+        init : function () {
+            var isStorageAvailable = function (storage) {
+                if (typeof storage == 'undefined') return false;
+                try { // hack for safari incognito
+                    storage.setItem("storage", "");
+                    storage.getItem("storage");
+                    storage.removeItem("storage");
+                    return true;
+                } catch (err) {
+                    return false;
+                }
+            };
+
+            if (!isStorageAvailable(window.localStorage) || isStorageAvailable(window.sessionStorage))
+                (function () {
+
+                    var Storage = function (type) {
+                        function createCookie(name, value, days) {
+                            var date, expires;
+
+                            if (days) {
+                                date = new Date();
+                                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                                expires = "; expires=" + date.toGMTString();
+                            } else {
+                                expires = "";
+                            }
+                            document.cookie = name + "=" + value + expires + "; path=/";
+                        }
+
+                        function readCookie(name) {
+                            var nameEQ = name + "=",
+                                ca = document.cookie.split(';'),
+                                i, c;
+
+                            for (i = 0; i < ca.length; i++) {
+                                c = ca[i];
+                                while (c.charAt(0) == ' ') {
+                                    c = c.substring(1, c.length);
+                                }
+
+                                if (c.indexOf(nameEQ) == 0) {
+                                    return c.substring(nameEQ.length, c.length);
+                                }
+                            }
+                            return null;
+                        }
+
+                        function setData(data) {
+                            data = JSON.stringify(data);
+                            if (type == 'session') {
+                                window.name = data;
+                            } else {
+                                createCookie('localStorage', data, 365);
+                            }
+                        }
+
+                        function clearData() {
+                            if (type == 'session') {
+                                window.name = '';
+                            } else {
+                                createCookie('localStorage', '', 365);
+                            }
+                        }
+
+                        function getData() {
+                            var data = type == 'session' ? window.name : readCookie('localStorage');
+                            return data ? JSON.parse(data) : {};
+                        }
+
+
+                        // initialise if there's already data
+                        var data = getData();
+
+                        return {
+                            length: 0,
+                            clear: function () {
+                                data = {};
+                                this.length = 0;
+                                clearData();
+                            },
+                            getItem: function (key) {
+                                return data[key] === undefined ? null : data[key];
+                            },
+                            key: function (i) {
+                                // not perfect, but works
+                                var ctr = 0;
+                                for (var k in data) {
+                                    if (ctr == i) return k;
+                                    else ctr++;
+                                }
+                                return null;
+                            },
+                            removeItem: function (key) {
+                                if (data[key] === undefined) this.length--;
+                                delete data[key];
+                                setData(data);
+                            },
+                            setItem: function (key, value) {
+                                if (data[key] === undefined) this.length++;
+                                data[key] = value + ''; // forces the value to a string
+                                setData(data);
+                            }
+                        };
+                    };
+
+                    if (!isStorageAvailable(window.localStorage))  window.localStorage = new Storage('local');
+                    if (!isStorageAvailable(window.sessionStorage)) window.sessionStorage = new Storage('session');
+
+                })();
+        }
+    };
+
+    return webStoragePolyfill;
+});
---
+++ src/web-storage.js
@@ -0,0 +1,155 @@
+/**                   _
+ *  _             _ _| |_
+ * | |           | |_   _|
+ * | |___  _   _ | | |_|
+ * | '_  \| | | || | | |
+ * | | | || |_| || | | |
+ * |_| |_|\___,_||_| |_|
+ *
+ * (c) Huli Inc
+ */
+
+/**
+ * @file WebStorage Wrapper
+ * @description WebStorage Wrapper, uses localStorage when available, and falls back on cookies.
+ * Triggers an event if there is a change on a web storage key.
+ * @requires jquery
+ * @requires ju-shared/observable-class
+ * @requires ju-shared/util
+ * @requires ju-shared/lib/vendor/polyfills/webstorage-polyfill
+ * @module ju-shared/web-storage
+ * @fires storageEvent
+ * @extends ju-shared/observable-class
+ */
+
+define([
+        'jquery',
+        'ju-shared/observable-class',
+        'ju-shared/util',
+        'ju-shared/lib/vendor/polyfills/webstorage-polyfill'
+    ],
+    function (
+        $,
+        ObservableClass,
+        Util,
+        WebStoragePolyfill
+    ){
+
+        'use strict';
+
+        var WebStorage = ObservableClass.extend({
+
+            /**
+             * @constructor
+             * @alias module:ju-shared/web-storage
+             * @param {Object} [opts] - configuration options
+             * @param {Boolean} [opts.useCookies=false] - force the use of cookies instead of web storage.
+             * @param {Boolean} [opts.fireStorageEvent=true] - activate the listener of storage event, it is fired when a storage area (localStorage or sessionStorage) has been modified.
+             */
+            init : function (opts) {
+                this.useCookies = false;
+                this.fireStorageEvent = true;
+                if (opts) {
+                    this.useCookies = opts.useCookies || this.useCookies;
+                    this.fireStorageEvent = opts.fireStorageEvent || this.fireStorageEvent;
+                }
+                this.isLocalStorageAvailable = this.getIsLocalStorageAvailable();
+                // use cookies if the browser doesn't support local storage
+                if ( this.useCookies || !this.isLocalStorageAvailable ){
+                    WebStoragePolyfill.init();
+                    this.useCookies = true;
+                }
+
+                if (this.isLocalStorageAvailable && this.fireStorageEvent){
+                    window.addEventListener("storage", this.storageEventHandler, false);
+                }
+            },
+
+            /**
+             * Sets the item in the storage
+             * @param {String} key - A String containing the name of the key you want to create/update.
+             * @param {String} value - A DOMString containing the value you want to give the key you are creating/updating.
+             */
+            setItem : function (key, value) {     // jshint ignore:line
+                window.localStorage.setItem(key, value);
+            },
+
+            /**
+             * Gets the item from the storage
+             * @param {String} key - A String containing the name of the key you want to create/update.
+             * @return {String} value for the given key
+             */
+            getItem : function (key) {
+                return window.localStorage.getItem(key);
+            },
+
+            /**
+             * Removes the item from the storage
+             * @param {String} key - A String containing the name of the key you want to create/update.
+             */
+            removeItem : function (key) {
+                window.localStorage.removeItem(key);
+            },
+
+            /***
+             * Check if localStorage is natively supported
+             * @returns {Boolean}
+             */
+            getIsLocalStorageAvailable : function() {
+                var storage = window.localStorage,
+                    storageAvailable = false;
+                try {
+                    storage.setItem('testKey', '1');
+                    storage.removeItem('testKey');
+                    storageAvailable = true;
+                } catch (e) {
+                    log('localStorage is not available');
+                }
+
+                return storageAvailable;
+            },
+            /**
+             * Indicates if the library is using native localStorage(true) otherwise will use cookies
+             * @returns {Boolean}
+             */
+            isNativeSupport : function(){
+                return this.isLocalStorageAvailable;
+            },
+
+            /**
+             * Clear local storage data
+             */
+            deleteLocalStorage : function() {
+                window.localStorage.clear();
+            },
+
+            /**
+             * Event handler for storage events
+             * @param event {StorageEvent} - event {@link https://developer.mozilla.org/en-US/docs/Web/Events/storage}
+             * @todo implement an event listener for storage key, right now fires an event for a change in any key
+             */
+            storageEventHandler : function (event) {
+                /**
+                 * @event storageEvent
+                 * @type {Object}
+                 * @see https://developer.mozilla.org/en-US/docs/Web/Events/storage
+                 */
+                this.trigger(this.EV.STORAGE_EVENT, event);
+            }
+        });
+
+        WebStorage.classMembers({
+            /**
+             * @constant {String} EV.STORAGE_EVENT - storage event name
+             */
+            EV : {
+                STORAGE_EVENT : 'storageEvent'
+            },
+            formatKey : function () {
+                return Util.format.apply(Util, arguments);
+            }
+        });
+
+        return WebStorage;
+
+    });
